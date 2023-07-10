4031885b007e6925860d0d30109f27192075dd7b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-10 21:40:47 +0200

    add crud tests for employee final

--- models/test/employee.crud.test.js
+++ models/test/employee.crud.test.js
@@ -14,7 +14,7 @@ describe('Employee', () => {
     }
   });
   describe('Reading data', () => {
-    before(async () => {
+    beforeEach(async () => {
       const testEmpOne = new Employee({
         firstName: 'Employee #1',
         lastName: 'LastName #1',
@@ -29,25 +29,29 @@ describe('Employee', () => {
       });
       await testEmpTwo.save();
     });
+
+    afterEach(async () => {
+      await Employee.deleteMany();
+    });
+
     it('should return all the data with "find" method', async () => {
       const employees = await Employee.find();
       const expectedLength = 2;
       expect(employees.length).to.be.equal(expectedLength);
     });
-    after(async () => {
-      await Employee.deleteMany();
-    });
+
     it('should return proper document by various params with findOne method.', async () => {
       const employee = await Employee.findOne({ firstName: 'Employee #1' });
       const expectedFirstName = 'Employee #1';
       expect(employee.firstName).to.be.equal(expectedFirstName);
     });
-    after(async () => {
-      await Employee.deleteMany();
-    });
   });
 
   describe('Creating data', () => {
+    afterEach(async () => {
+      await Employee.deleteMany();
+    });
+
     it('should insert new document with insertOne method', async () => {
       const employee = new Employee({
         firstName: 'Employee #3',
@@ -57,10 +61,8 @@ describe('Employee', () => {
       await employee.save();
       expect(employee.isNew).to.be.false;
     });
-    after(async () => {
-      await Employee.deleteMany();
-    });
   });
+
   describe('Updating data', () => {
     beforeEach(async () => {
       const testEmpOne = new Employee({
@@ -77,6 +79,11 @@ describe('Employee', () => {
       });
       await testEmpTwo.save();
     });
+
+    afterEach(async () => {
+      await Employee.deleteMany();
+    });
+
     it('should properly update one document with updateOne method', async () => {
       await Employee.updateOne(
         {
@@ -89,6 +96,7 @@ describe('Employee', () => {
       });
       expect(updatedEmployee).to.not.be.null;
     });
+
     it('should properly update one document with save method,', async () => {
       const employee = await Employee.findOne({ firstName: 'Employee #1' });
       employee.firstName = '=Employee #1=';
@@ -99,15 +107,14 @@ describe('Employee', () => {
       });
       expect(updatedEmployee).to.not.be.null;
     });
+
     it('should properly update multiple documents with updateMany method', async () => {
       await Employee.updateMany({}, { $set: { firstName: 'Updated!' } });
       const employee = await Employee.find({ firstName: 'Updated!' });
       expect(employee.length).to.be.equal(2);
     });
-    afterEach(async () => {
-      await Employee.deleteMany();
-    });
   });
+
   describe('Removing data', () => {
     beforeEach(async () => {
       const testEmpOne = new Employee({
@@ -124,6 +131,11 @@ describe('Employee', () => {
       });
       await testEmpTwo.save();
     });
+
+    afterEach(async () => {
+      await Employee.deleteMany();
+    });
+
     it('should properly remove one document with deleteOne method', async () => {
       const employee = await Employee.findOne({ firstName: 'Employee #1' });
       await employee.remove();
@@ -132,21 +144,20 @@ describe('Employee', () => {
       });
       expect(removedEmployee).to.be.null;
     });
+
     it('should properly remove one document with remove method', async () => {
-      const employee = Employee.findOne({ name: 'Employee #1' });
+      const employee = Employee.findOne({ firstName: 'Employee #1' });
       await employee.remove();
       const removeEmployee = await Employee.findOne({
         firstName: 'Employee #1',
       });
       expect(removeEmployee).to.be.null;
     });
+
     it('should properly remove multiple documents with deleteMany method', async () => {
       await Employee.deleteMany();
       const employee = await Employee.find();
       expect(employee.length).to.be.equal(0);
     });
-  });
-  afterEach(async () => {
-    await Employee.deleteMany();
   });
 });
---
+++ test/api/departments/get.test.js
@@ -0,0 +1,26 @@
+// const expect = require('chai').expect;
+// const chaiHttp = require('chai-http');
+// const server = require('../../server.js');
+
+// chai.use(chaiHttp);
+
+// const expect = chai.expect;
+// const request = chai.request;
+
+// const NODE_ENV = process.env.NODE_ENV;
+// let dbUri = '';
+
+// if (NODE_ENV === 'production') dbUri = 'url to remote db';
+// else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
+// else dbUri = 'mongodb://localhost:27017/companyDB';
+
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
+
+// describe('GET /api/departments', () => {
+//   it('/ should return all departments', () => {});
+
+//   it('/:id should return one department by :id ', () => {});
+
+//   it('/random should return one random department', () => {});
+// });
