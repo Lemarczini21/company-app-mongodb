ffe7686923d7e71521fbafb2188702560addc350
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-10 22:16:46 +0200

    Add configuration for endpoint test

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,26 +1,15 @@
-// const expect = require('chai').expect;
-// const chaiHttp = require('chai-http');
-// const server = require('../../server.js');
+const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const server = require('../../../server.js');
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
+const request = chai.request;
 
-// const expect = chai.expect;
-// const request = chai.request;
+describe('GET /api/departments', () => {
+  it('/ should return all departments', () => {});
 
-// const NODE_ENV = process.env.NODE_ENV;
-// let dbUri = '';
+  it('/:id should return one department by :id ', () => {});
 
-// if (NODE_ENV === 'production') dbUri = 'url to remote db';
-// else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
-// else dbUri = 'mongodb://localhost:27017/companyDB';
-
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-
-// describe('GET /api/departments', () => {
-//   it('/ should return all departments', () => {});
-
-//   it('/:id should return one department by :id ', () => {});
-
-//   it('/random should return one random department', () => {});
-// });
+  it('/random should return one random department', () => {});
+});
